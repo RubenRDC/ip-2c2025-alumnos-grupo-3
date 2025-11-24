4a211f8c312465dc84d7159c0a7d6ecc2ed08611
Aaron Orbegozo <aaronorbegozo4@gmail.com>
2025-11-23 23:19:21 -0300

    Agrego el insertion_sort y el selection_sort ya terminados

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -21,8 +21,56 @@ def step():
     # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
     #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
     #   Al terminar el barrido, pasar a fase "swap".
+
     # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
     #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
     #
     # Cuando i llegue al final, devolvé {"done": True}.
-    return {"done": True}
+
+    global items, n, i, j, min_idx, fase
+
+    # Si i+1 es igual a la lista, ya está todo ordenado
+    if(i+1==n):
+        return {"done":True}
+    
+    # ------------------ FASE "BUSCAR" ------------------
+    if(fase == "buscar"):
+
+        # Si encontramos un valor más chico, actualizamos min_idx
+        if(items[j]<items[min_idx]):
+
+            min_idx = j
+            return {"a": min_idx, "b": j, "swap": False, "done": False}
+
+        # Si no es menor, solo avanzamos j
+        j=j+1
+
+        # Si j llegó al final, pasamos a fase "swap"
+        if(j>=n):
+            fase="swap"
+        return {"a": min_idx, "b": j, "swap": False, "done":False}
+    
+    # ------------------ FASE "SWAP" ------------------
+    if(fase=="swap"):
+
+        minAux = min_idx # guardamos el índice del mínimo encontrado
+        iAux = i # guardamos la posición donde debe colocarse
+
+        # Si el mínimo encontrado NO está en la posición correcta, hacemos el intercambio
+        if(items[minAux]!=items[iAux]):
+            itemsAux = items[i]         # guardamos temporalmente items[i]
+            items[min_idx] = items[i]   # movemos el valor actual a la posición del mínimo
+            items[i] = itemsAux         # colocamos el mínimo en su lugar
+            min_idx = i                 # el mínimo ahora quedó en la posición i
+            return {"a": iAux, "b": minAux, "swap": True, "done": False}
+        
+        # Si no hubo swap (el mínimo ya estaba donde debía):
+        i = i+1         # avanzamos a la siguiente posición a ordenar
+        j = i+1         # el nuevo recorrido empieza desde i+1
+        min_idx = i     # asumimos que el nuevo mínimo arranca en i
+        fase = "buscar" # volvemos a buscar
+        return {"done":False}
+
+
+
+
